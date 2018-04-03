f791fd05e25a988ab385e675c99dd07bccc45cbe
pittsblake <pittsblake1@gmail.com>
2018-04-03 10:05:47 -0400

    Created index,show,create, and delete routes for topics controller

--- app/controllers/api/topics_controller.rb
+++ app/controllers/api/topics_controller.rb
@@ -1,12 +1,29 @@
 class Api::TopicsController < ApplicationController
   def index
-    @topics = Title.all
+    @topics = Topic.all
     render json: @topics
   end
 
   def show
+    @topic = Topic.find(params[:id])
+    render json: @topic
   end
 
   def create
+    @topic = Topic.new(topic_params)
+
+    @topic.save!
+    render json: @topic
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id]).destroy
+    render json: {msg: "Deleted!"}
+  end
+
+
+  private 
+  def topic_params
+    params.require(:topic).permit(:title)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
    namespace :api do
-    resources :titles do 
+    resources :topics do 
       resources :definitions do
         resources :ratings 
       end
